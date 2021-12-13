0dda026dadb6844d540fef0f2b112b5594c0ea57
SimonDaNinja <simon@simonssoffa.xyz>
2021-12-13 23:13:09 +0100

    December 11th (I'm behind a bit :( )

---
+++ 11/part_one.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python3
+
+# just cat the input file and pipe it to this program
+
+from pprint import pprint
+
+def getNeighbours(octopusMap, i, j):
+    neighbours = []
+    if i > 0:
+        neighbours.append((i-1, j))
+        if j > 0:
+            neighbours.append((i-1, j-1))
+        if j < 9:
+            neighbours.append((i-1, j+1))
+    if i < 9:
+        neighbours.append((i+1, j))
+        if j > 0:
+            neighbours.append((i+1, j-1))
+        if j < 9:
+            neighbours.append((i+1, j+1))
+    if j > 0:
+        neighbours.append((i, j-1))
+    if j < 9:
+        neighbours.append((i, j+1))
+    return neighbours
+
+def printMap(octopusMap):
+    printStr = "\n".join(["".join([(str(i) if i != 0 else "*") + " " for i in line]) for line in octopusMap])
+    print(printStr)
+
+def flash(octopusMap, i, j, flashed):
+    nFlashes = 1
+    flashed.append((i,j))
+    for neighbour in getNeighbours(octopusMap, i, j):
+        octopusMap[neighbour[0]][neighbour[1]] += 1
+        if octopusMap[neighbour[0]][neighbour[1]] > 9 and neighbour not in flashed:
+            nFlashes += flash(octopusMap, neighbour[0], neighbour[1], flashed)
+    return nFlashes
+
+if __name__ == '__main__':
+    lines = []
+    while True:
+        try:
+            cols = [int(i) for i in input()]
+            lines.append(cols)
+        except EOFError:
+            break
+
+    nFlashes = 0
+    for iteration in range(100):
+        flashed = []
+        for i in range(10):
+            for j in range(10):
+                lines[i][j] += 1
+                if lines[i][j] > 9 and (i,j) not in flashed:
+                    nFlashes += flash(lines, i, j, flashed)
+        for i in range(10):
+            for j in range(10):
+                if lines[i][j] > 9:
+                    lines[i][j] = 0
+    print(nFlashes)
